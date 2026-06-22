96eca9213c936bff2ab06540247734e979f505e5
Minuri Viranga <141044217+minu2003@users.noreply.github.com>
2026-06-22 16:29:42 +0530

    new updates

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "encrypted-tbn0.gstatic.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
   outputFileTracingIncludes: {
